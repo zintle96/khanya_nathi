e5027321f65fc45e51c3c66c1ef1919760d6780e
Zintle Meslane <zintle.meslane@dadevs.co.za>
2022-03-11 16:15:04 +0200

    server

--- youth/server.js
+++ youth/server.js
@@ -1,16 +1,9 @@
-//Install express server
-const express = require('express');
-const path = require('path');
-
+const express = require('express')
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/youth'));
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/youth/index.html'));
+app.use(express.static('./dist/youth'));
+app.get('/*', function (req, res) {
+    res.sendFile('index.html', { root: 'dist' }
+    );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
